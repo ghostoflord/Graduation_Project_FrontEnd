fc5b1985455ffd26b38c9f5f0aa5b84a388b4863
ghostoflord <ghost2892003zzz@gmail.com>
2025-04-15 18:00:29 +0700

    design login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App.tsx'
+import LoginPage from './pages/admin/login/login.page.tsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
-)
+);
